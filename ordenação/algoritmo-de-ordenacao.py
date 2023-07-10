6872b702d228232c43ea73e0ede730361a94d8b0
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2023-07-10 16:25:22 -0300

    update

--- ordenação/algoritmo-de-ordenacao.py
+++ ordenação/algoritmo-de-ordenacao.py
@@ -1,5 +1,6 @@
 class Ordenador:
     def bubbleSort(self,lista,reverse=False):
+        ''' (list,True/False) -> Compara o maior ou menor elemento entre um elemento da lista e seu sucessor, reorganizando a lista.  '''
 
         for i in range(len(lista)-1):
             for j in range(i+1,len(lista)):
@@ -9,10 +10,14 @@ class Ordenador:
                     if (lista[j]<lista[i]):
                         lista[j],lista[i]=lista[i],lista[j]
 
-        print(lista)
+        return lista
 
-lista=[5,3,1,2,7,6]
-x=Ordenador()
-y=Ordenador()
-x.bubbleSort(lista) # [1, 2, 3, 5, 6, 7]
-y.bubbleSort(lista,True) # [7, 6, 5, 3, 2, 1]
+
+if __name__=='__main__':
+
+    x=Ordenador()
+    y=Ordenador()
+
+    lista=[5,3,1,2,7,6] # Lista com os elementos sem uma ordem definida
+    x.bubbleSort(lista) # Lista Ordenada Crescentemente: [1, 2, 3, 5, 6, 7]
+    y.bubbleSort(lista,True) # Lista Ordenada Decrescentemente: [7, 6, 5, 3, 2, 1]
