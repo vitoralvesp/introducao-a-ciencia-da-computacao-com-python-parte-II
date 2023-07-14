39050a9cf55d8bdb5117e3112217ba6931aa3da4
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2023-07-14 16:50:43 -0300

    ex3 update

--- ordenação/exercícios/ex3.py
+++ ordenação/exercícios/ex3.py
@@ -1,22 +1,28 @@
-class Buscador:
-    def BuscaBinaria(self,lista,elemento):
-        ''' Retorna o índice de um elemento caso exista no array.
-            Se não estiver, retorna False. '''
+def busca(lista,elemento):
+    ''' Retorna o índice de um elemento caso exista no array.
+        Se não estiver, retorna False. '''
         
-        primeiro=0
-        ultimo=len(lista)-1
+    primeiro=0
+    ultimo=len(lista)-1
 
-        while (primeiro<=ultimo):
-            meio=(primeiro+ultimo)//2
-            if (lista[meio]==elemento):
-                return meio
+    while (primeiro<=ultimo):
+        meio=(primeiro+ultimo)//2
+        print(meio)
+
+        if (lista[meio]==elemento):
+            return meio
+        else:
+            if (elemento<lista[meio]):
+                ultimo=meio-1
             else:
-                if (elemento<lista[meio]):
-                    ultimo=meio-1
-                else:
-                    primeiro=meio+1
+                primeiro=meio+1
         
-        return False
+    return False
+
+# print(busca(['a','e','i'],'e'))
+# print(busca([1,2,3,4,5],6))
+# print(busca([1,2,3,4,5,6],4))
+# print(busca([1,2,3,4,5,6],4))
 
 
 
