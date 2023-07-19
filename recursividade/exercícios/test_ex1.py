b43b1d11e8d77212ba044c4af729fa431ddd034d
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2023-07-19 10:17:33 -0300

    update

--- recursividade/exercícios/test_ex1.py
+++ recursividade/exercícios/test_ex1.py
@@ -2,7 +2,11 @@ from ex1 import soma_lista
 import pytest
 
 @pytest.mark.parametrize('lista,esperado', [
-    ([1,2],3)
+    ([1,2],3),
+    ([1,2,3],6),
+    ([1,2,3,4],10),
+    ([1,2,3,4,5],15),
+    ([5,5,5,5],20)
 ])
 
 def test_soma_lista(lista,esperado):
